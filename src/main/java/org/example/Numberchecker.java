dc88c42b49095fdd51e4ea45f9fb69f8db275782
Rudin45 <89311805+Rudin45@users.noreply.github.com>
2025-03-28 14:43:48 -0600

    Agregado input desde consola

--- src/main/java/org/example/Numberchecker.java
+++ src/main/java/org/example/Numberchecker.java
@@ -1,11 +1,15 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Numberchecker {
     public static boolean isEven(int number){
         return number % 2==0;
     }
     public static void main(String[] args) {
-        int number = 8; // cámbialo para probar
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingresa un número: ");
+        int number = scanner.nextInt();
         System.out.println("¿Es par? " + isEven(number));
     }
 }
